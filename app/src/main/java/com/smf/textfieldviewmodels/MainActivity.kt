7bf92d03ce14ab2abfe216c94472c885ab0fe8e1
Shawn Flynn <ShawnM.Flynn0@gMail.com>
2023-08-23 17:29:53 -0400

    Initial commit

---
+++ app/src/main/java/com/smf/textfieldviewmodels/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.smf.textfieldviewmodels
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.smf.textfieldviewmodels.ui.theme.TextFieldViewModelsTheme
+
+class MainActivity : ComponentActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContent {
+			TextFieldViewModelsTheme {
+				// A surface container using the 'background' color from the theme
+				Surface(
+					modifier = Modifier.fillMaxSize(),
+					color = MaterialTheme.colorScheme.background
+				) {
+				}
+			}
+		}
+	}
+}
